2541cf22d18f3de3be2a5d24c7bba76a6cd23cc4
Dave Donaghy <dave.donaghy+github@pm.me>
2018-08-12 17:04:46 +0100

    Initial file samples

---
+++ dump.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+from pymongo import MongoClient
+import sys
+
+parser = argparse.ArgumentParser(description='List onjects in a named Mongo database and collection.')
+parser.add_argument('-s', '--server', nargs=1, help='Mongo server URL')
+parser.add_argument('database', nargs=1, help='database name')
+parser.add_argument('collection', nargs=1, help='collection name')
+
+args = parser.parse_args()
+
+if args.server is None:
+    client = MongoClient()
+else:
+    client = MongoClient(args.server)
+
+database = client[args.database[0]]
+
+collection = database[args.collection[0]]
+for item in collection.find():
+    print(item)
---
+++ insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+from pymongo import MongoClient
+import sys
+
+parser = argparse.ArgumentParser(description='Insert JSON object from file into a named Mongo database and collection.')
+parser.add_argument('-s', '--server', nargs=1, help='Mongo server URL')
+parser.add_argument('database', nargs=1, help='database name')
+parser.add_argument('collection', nargs=1, help='collection name')
+parser.add_argument('datafile', nargs=1, help='datafile name - this file must contains a JSON map')
+
+args = parser.parse_args()
+
+if args.server is None:
+    client = MongoClient()
+else:
+    client = MongoClient(args.server)
+
+database = client[args.database[0]]
+
+with open(args.datafile[0]) as data_file:
+    data = json.load(data_file)
+
+    collection = database[args.collection[0]]
+    inserted_id = collection.insert_one(data).inserted_id
+    print('Inserted data and got id {}'.format(inserted_id))
